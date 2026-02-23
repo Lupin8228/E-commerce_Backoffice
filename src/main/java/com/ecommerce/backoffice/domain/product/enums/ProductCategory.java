2a2fa5ce01d8c5e463cb6ad914c353aa97c67658
sosugyeong <sugyeong247@gmail.com>
2026-02-23 19:25:49 +0900

    feat: 한글 Enum 매핑용 WebConfig 추가

--- src/main/java/com/ecommerce/backoffice/domain/product/enums/ProductCategory.java
+++ src/main/java/com/ecommerce/backoffice/domain/product/enums/ProductCategory.java
@@ -1,5 +1,8 @@
 package com.ecommerce.backoffice.domain.product.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ProductCategory {
     ELECTRONICS("전자기기"),
     FASHION("의류"),
@@ -9,7 +12,20 @@ public enum ProductCategory {
     ProductCategory(String categoryValue) {
         this.categoryValue = categoryValue;
     }
+
+    @JsonValue //객체 -> JSON. db에서 꺼내서 JSON으로 응답을 줄 때 문자열로 보여주게 함
     public String getCategoryValue() {
         return categoryValue;
     }
+
+    @JsonCreator //JSON에 문자열이 들어오면 객체로 변환해줌
+    public static ProductCategory fromValue(String value){
+        for (ProductCategory category : ProductCategory.values()) {
+            if (category.categoryValue.equals(value)) {
+                return category;
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/ecommerce/backoffice/domain/product/enums/ProductStatus.java
+++ src/main/java/com/ecommerce/backoffice/domain/product/enums/ProductStatus.java
@@ -1,5 +1,8 @@
 package com.ecommerce.backoffice.domain.product.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ProductStatus {
     ON_SALE("판매중"),
     OUT_OF_STOCK("품절"),
@@ -9,7 +12,19 @@ public enum ProductStatus {
     ProductStatus(String statusValue) {
         this.statusValue = statusValue;
     }
+
+    @JsonValue //객체 -> JSON시 "판매중"으로 출력되게 함
     public String getStatusValue() {
         return statusValue;
+    }
+
+    @JsonCreator
+    public static ProductStatus fromValue(String value) {
+        for (ProductStatus status : ProductStatus.values()) {
+            if (status.statusValue.equals(value)) {
+                return status;
+            }
+        }
+        return null;
     }
 }
