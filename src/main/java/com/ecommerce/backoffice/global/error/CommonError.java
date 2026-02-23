72e6b2b3a756718755e0239fc6149910b724576c
Max-1012 <thdud001012@gmail.com>
2026-02-23 15:44:36 +0900

    Refactor: CommonError 파일 수정 전으로 복구

--- src/main/java/com/ecommerce/backoffice/global/error/CommonError.java
+++ src/main/java/com/ecommerce/backoffice/global/error/CommonError.java
@@ -51,10 +51,8 @@ public enum CommonError {
     FORBIDDEN_SUPER_ADMIN_ONLY(FORBIDDEN, "A201", "슈퍼 관리자만 접근할 수 있습니다."),
     INVALID_UPDATE_REQUEST(BAD_REQUEST, "A302", "수정할 값이 없습니다."),
     ADMIN_NOT_PENDING(BAD_REQUEST, "A401", "승인대기 상태에서만 처리할 수 있습니다."),
-    CURRENT_PASSWORD_MISMATCH(BAD_REQUEST, "A501", "현재 비밀번호가 일치하지 않습니다."),
+    CURRENT_PASSWORD_MISMATCH(BAD_REQUEST, "A501", "현재 비밀번호가 일치하지 않습니다.");
 
-    // -- 3000 : Customer --
-    CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND, "A3101", "존재하지 않는 손님입니다.");
 
     private final HttpStatus status;
     private final String code;
