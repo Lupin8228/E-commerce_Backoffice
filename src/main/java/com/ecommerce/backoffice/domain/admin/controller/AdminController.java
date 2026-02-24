eb3b3b2db2ae187f4ccd5428bfa1ffc9458fc68b
최길중 <choil8228@gmail.com>
2026-02-24 11:43:24 +0900

    fix: 로직  session -> jwt 변경

--- src/main/java/com/ecommerce/backoffice/domain/admin/controller/AdminController.java
+++ src/main/java/com/ecommerce/backoffice/domain/admin/controller/AdminController.java
@@ -3,16 +3,16 @@ package com.ecommerce.backoffice.domain.admin.controller;
 
 import com.ecommerce.backoffice.domain.admin.dto.request.*;
 import com.ecommerce.backoffice.domain.admin.dto.response.*;
-import com.ecommerce.backoffice.domain.admin.dto.session.SessionAdmin;
-import com.ecommerce.backoffice.domain.admin.dto.session.SessionKey;
 import com.ecommerce.backoffice.domain.admin.service.AdminService;
 import com.ecommerce.backoffice.global.common.ApiResponse;
+import com.ecommerce.backoffice.global.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -57,9 +57,9 @@ public class AdminController {
 
 
     // 관리자 리스트 조회
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @GetMapping("/admins")
     public ResponseEntity<ApiResponse<GetAdminListResponse>> getAllAdmins(
-            HttpSession session,
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false, defaultValue = "1") Integer page,
             @RequestParam(required = false, defaultValue = "10") Integer size,
@@ -68,115 +68,115 @@ public class AdminController {
             @RequestParam(required = false) String role,
             @RequestParam(required = false) String status
     ) {
-        GetAdminListResponse response = adminService.getAllAdmins(session, keyword, page, size, sortBy, sortDir, role, status);
+        GetAdminListResponse response = adminService.getAllAdmins(keyword, page, size, sortBy, sortDir, role, status);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 관리자 상세 조회
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @GetMapping("/{adminId}")
     public ResponseEntity<ApiResponse<GetAdminDetailResponse>> getOne(
-            HttpSession session,
             @PathVariable Long adminId
     ) {
-        GetAdminDetailResponse response = adminService.getOne(session, adminId);
+        GetAdminDetailResponse response = adminService.getOne(adminId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 관리자 정보 수정
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @PatchMapping("/{adminId}")
     public ResponseEntity<ApiResponse<UpdateAdminResponse>> updateAdminInfo(
-            HttpSession session,
             @PathVariable Long adminId,
             @Valid @RequestBody UpdateAdminRequest requestBody
     ) {
-        UpdateAdminResponse response = adminService.updateAdminInfo(session, adminId, requestBody);
+        UpdateAdminResponse response = adminService.updateAdminInfo(adminId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 관리자 역할 변경
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @PatchMapping("/{adminId}/role")
     public ResponseEntity<ApiResponse<UpdateAdminRoleResponse>> updateAdminRole(
-            HttpSession session,
             @PathVariable Long adminId,
             @Valid @RequestBody UpdateAdminRoleRequest requestBody
 
     ) {
-        UpdateAdminRoleResponse response = adminService.updateAdminRole(session, adminId, requestBody);
+        UpdateAdminRoleResponse response = adminService.updateAdminRole(adminId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 관리자 상태 변경
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @PatchMapping("/{adminId}/status")
     public ResponseEntity<ApiResponse<PatchAdminStatusChangeResponse>> changeStatus(
-            HttpSession session,
             @PathVariable Long adminId,
             @Valid @RequestBody PatchAdminStatusChangeRequest requestBody
 
     ) {
-        PatchAdminStatusChangeResponse response = adminService.changeStatus(session, adminId, requestBody);
+        PatchAdminStatusChangeResponse response = adminService.changeStatus(adminId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 관리자 삭제
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @DeleteMapping("/{adminId}")
     public ResponseEntity<ApiResponse<Void>> delete(
-            HttpSession session,
             @PathVariable Long adminId
     ) {
-        adminService.deleteAdmin(session, adminId);
+        adminService.deleteAdmin(adminId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(null));
     }
 
     // 관리자 승인
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @PatchMapping("/{adminId}/approve")
     public ResponseEntity<ApiResponse<PatchDecisionAdminResponse>> approve(
-            HttpSession session,
             @PathVariable Long adminId
     ) {
-        PatchDecisionAdminResponse response = adminService.approveAdmin(session, adminId);
+        PatchDecisionAdminResponse response = adminService.approveAdmin(adminId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 거부
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
     @PatchMapping("/{adminId}/reject")
     public ResponseEntity<ApiResponse<PatchDecisionAdminResponse>> reject(
-            HttpSession session,
             @PathVariable Long adminId,
             @Valid @RequestBody PatchRejectAdminRequest requestBody
     ) {
-        PatchDecisionAdminResponse response = adminService.rejectAdmin(session, adminId, requestBody);
+        PatchDecisionAdminResponse response = adminService.rejectAdmin(adminId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 내 프로필 조회
     @GetMapping("/me/profile")
     public ResponseEntity<ApiResponse<GetProfileResponse>> getProfile(
-            HttpSession session
+            @AuthenticationPrincipal UserDetailsImpl principal
     ) {
-        SessionAdmin me = SessionKey.getLoginAdmin(session);
-        GetProfileResponse response = adminService.getProfile(session, me.id());
+        Long meId = principal.getAdmin().getId();
+        GetProfileResponse response = adminService.getProfile(meId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 내 프로필 수정
     @PatchMapping("/me/profile")
     public ResponseEntity<ApiResponse<UpdateProfileResponse>> updateProfile(
-            HttpSession session,
+            @AuthenticationPrincipal UserDetailsImpl principal,
             @Valid @RequestBody UpdateProfileRequest requestBody
     ) {
-        SessionAdmin me = SessionKey.getLoginAdmin(session);
-        UpdateProfileResponse response = adminService.updateProfile(session, me.id(), requestBody);
+        Long meId = principal.getAdmin().getId();
+        UpdateProfileResponse response = adminService.updateProfile(meId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
     // 내 비밀 번호 변경
     @PatchMapping("/me/password")
     public ResponseEntity<ApiResponse<UpdatePasswordResponse>> changeMyPassword(
-            HttpSession session,
+            @AuthenticationPrincipal UserDetailsImpl principal,
             @Valid @RequestBody UpdatePasswordRequest requestBody
     ) {
-        SessionAdmin me = SessionKey.getLoginAdmin(session);
-        UpdatePasswordResponse response = adminService.changePassword(session, me.id(), requestBody);
+        Long meId = principal.getAdmin().getId();
+        UpdatePasswordResponse response = adminService.changePassword(meId, requestBody);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
     }
 
