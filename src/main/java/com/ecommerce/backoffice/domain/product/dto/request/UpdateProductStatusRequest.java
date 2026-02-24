bc7f2f99c9261984b649c3204e6bacdb5f780a0e
sosugyeong <sugyeong247@gmail.com>
2026-02-24 21:02:27 +0900

    fix: validation 메시지 수정

--- src/main/java/com/ecommerce/backoffice/domain/product/dto/request/UpdateProductStatusRequest.java
+++ src/main/java/com/ecommerce/backoffice/domain/product/dto/request/UpdateProductStatusRequest.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateProductStatusRequest(
-        @NotNull(message = "카테고리는 필수 입력 항목입니다.")
+        @NotNull(message = "상태는 필수 입력 항목입니다.")
         ProductStatus status,
 
-        @NotNull(message = "가격은 필수 입력 항목입니다.")
+        @NotNull(message = "재고는 필수 입력 항목입니다.")
         @Min(value = 0, message = "재고는 0 이상이어야 합니다.")
         int stock
 ) {
--- src/main/java/com/ecommerce/backoffice/domain/review/dto/request/CreateReviewRequest.java
+++ src/main/java/com/ecommerce/backoffice/domain/review/dto/request/CreateReviewRequest.java
@@ -8,7 +8,7 @@ public record CreateReviewRequest(
         @Max(value = 5, message = "1에서 5사이의 값만 입력해주세요.")
         Integer rating,
 
-        @NotBlank(message = "설명은 필수 입력 항목입니다.")
+        @NotBlank(message = "리뷰는 필수 입력 항목입니다.")
         @Size(min = 10, message = "리뷰는 10자 이상 입력해주세요.")
         String description
 ) {
